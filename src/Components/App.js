f37fb76199f11e5074f1979d7201f653f130f72e
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-25 12:35:16 -0400

    Component - Feed2

--- src/Components/App.js
+++ src/Components/App.js
@@ -36,7 +36,7 @@ export default () => {
 				<GlobalStyles />
 				<Router>
 					<>
-						<Header />
+						{isLoggedIn && <Header />}
 						<Wrapper>
 							<Routes isLoggedIn={isLoggedIn} />
 							<Footer />
