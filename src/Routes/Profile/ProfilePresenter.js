fa4d12d9f4213897d35f0456c207b95574084843
PureDevPer <wooseok.kim1207@gmail.com>
2019-08-26 15:43:36 -0400

    Profile Screen

--- src/Routes/Profile/ProfilePresenter.js
+++ src/Routes/Profile/ProfilePresenter.js
@@ -8,15 +8,59 @@ import FollowButton from '../../Components/FollowButton';
 import SquarePost from '../../Components/SquarePost';
 
 const Wrapper = styled.div`
-	min-height: 60vh;
+	min-height: 100vh;
 `;
 
-const Header = styled.header``;
+const Header = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+	width: 80%;
+	margin: 0 auto;
+	margin-bottom: 40px;
+`;
 
 const HeaderColumn = styled.div``;
 
+const UsernameRow = styled.div`
+	display: flex;
+	align-items: center;
+`;
+
+const Username = styled.span`
+	font-size: 26px;
+	display: block;
+`;
+
+const Counts = styled.ul`
+	display: flex;
+	margin: 15px 0px;
+`;
+
+const Count = styled.li`
+	font-size: 16px;
+	&:not(:last-child) {
+		margin-right: 10px;
+	}
+`;
+
+const FullName = styled(FatText)`
+	font-size: 16px;
+`;
+
+const Bio = styled.p`
+	margin: 10px 0px;
+`;
+
+const Posts = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 200px);
+	grid-template-rows: 200px;
+	grid-auto-rows: 200px;
+`;
+
 export default ({ loading, data }) => {
-	if (loading) {
+	if (loading === true) {
 		return (
 			<Wrapper>
 				<Loader />
@@ -25,6 +69,7 @@ export default ({ loading, data }) => {
 	} else if (!loading && data && data.seeUser) {
 		const {
 			seeUser: {
+				id,
 				avatar,
 				username,
 				fullName,
@@ -38,13 +83,47 @@ export default ({ loading, data }) => {
 			}
 		} = data;
 		return (
-			<>
+			<Wrapper>
+				<Helmet>
+					<title>{username} | Prismagram</title>
+				</Helmet>
 				<Header>
 					<HeaderColumn>
 						<Avatar size="lg" url={avatar} />
 					</HeaderColumn>
+					<HeaderColumn>
+						<UsernameRow>
+							<Username>{username}</Username>{' '}
+							{!isSelf && <FollowButton isFollowing={isFollowing} id={id} />}
+						</UsernameRow>
+						<Counts>
+							<Count>
+								<FatText text={String(postsCount)} /> posts
+							</Count>
+							<Count>
+								<FatText text={String(followersCount)} /> followers
+							</Count>
+							<Count>
+								<FatText text={String(followingCount)} /> following
+							</Count>
+						</Counts>
+						<FullName text={fullName} />
+						<Bio>{bio}</Bio>
+					</HeaderColumn>
 				</Header>
-			</>
+				<Posts>
+					{posts &&
+						posts.map(post => (
+							<SquarePost
+								key={post.id}
+								likeCount={post.likeCount}
+								commentCount={post.commentCount}
+								file={post.files[0]}
+							/>
+						))}
+				</Posts>
+			</Wrapper>
 		);
 	}
+	return null;
 };
